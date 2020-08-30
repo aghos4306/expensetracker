b76f63b2f78aa67a4f2240021e19c9f4884d9b83
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-08-30 09:54:54 +0300

    TransactionList component setup

---
+++ src/components/TransactionLIst.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TransactionLIst = () => {
+  return (
+    <div>
+      <h3>History</h3>
+      <ul id="list" className="list">
+        <li className="minus">
+          Cash <span>-£400</span>
+          <button className="delete-btn">X</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default TransactionLIst;
