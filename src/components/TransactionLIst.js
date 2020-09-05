a05f3855e71f3ff1fa30716de3128ada9c34e58d
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-09-05 08:16:16 +0300

    transactions outputted

--- src/components/TransactionLIst.js
+++ src/components/TransactionLIst.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const TransactionLIst = () => {
+  //transactions is an arry, map through and display into li as separate component
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <div>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-£400</span>
-          <button className="delete-btn">X</button>
-        </li>
+        {transactions.map((transaction) => (
+          <li className="minus">
+            {transaction.text} <span>-£400</span>
+            <button className="delete-btn">X</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
